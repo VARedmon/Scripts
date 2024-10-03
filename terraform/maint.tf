0ed683d05f00aba811cc53f1d4ff2ff43193d7fa
varedmon <vincent.redmon@gmail.com>
2024-10-03 19:25:30 -0400

    updates

--- terraform/maint.tf
+++ terraform/maint.tf
@@ -1,6 +1,6 @@
 resource local_file name {
-  sensitive_content = ""
-  filename             = "${path.module}/files/outputfile"
+  filename             = "~/test.txt"
+  content = "This is a test file!"
   file_permission      = 0777
   directory_permission = 0777
 }
---
+++ terraform/variables.tf
@@ -0,0 +1,5 @@
+variable pet_name {
+  type        = string
+  default     = "Bubbles"
+  description = "My pet's name is bubbles"
+}
