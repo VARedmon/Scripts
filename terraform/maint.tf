5470f59bf0e55790a56ec32bf967459362a5d493
varedmon <vincent.redmon@gmail.com>
2024-10-04 09:24:26 -0400

    updates to terraform files

--- terraform/maint.tf
+++ terraform/maint.tf
@@ -1,6 +1,10 @@
-resource local_file name {
-  filename             = "~/test.txt"
-  content = "This is a test file!"
-  file_permission      = 0777
-  directory_permission = 0777
+resource local_file "test" {
+  filename = var.filename
+  content = var.file-content["statement1"]
+  
+}
+resource "random_pet" "my_pet" {
+  prefix = var.prefix[0]
+  separator = var.separator
+  length = var.length
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,5 +1,27 @@
-variable pet_name {
-  type        = string
-  default     = "Bubbles"
-  description = "My pet's name is bubbles"
+variable "filename" {
+  default = "~/test.txt"
+  type = string
+  
+}
+variable "file-content" {
+  type = map
+  default = {
+    "statement1" = "We love pets!"
+    "statement2" = "We love animals!"
+  }
+}
+variable "prefix" {
+  default = ["Mr", "Mrs", "Sir"]
+  type = list
+
+}
+variable "separator" {
+  default = "."
+  type = string
+
+}
+variable "length" {
+  default = "1"
+  type = string
+  
 }
