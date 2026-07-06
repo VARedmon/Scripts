1ac0288e2d6dca14683100a77b42fafcd36d4b82
vinnie <vincent.redmon@gmail.com>
2026-07-06 16:06:38 -0400

    updated testing

--- go/src/cards/deck_test.go
+++ go/src/cards/deck_test.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"math/rand"
 	"os"
-	"reflect"
 	"testing"
 )
 
@@ -23,58 +21,17 @@ func TestNewDeck(t *testing.T) {
 	}
 }
 
-func TestDeal(t *testing.T) {
-	cards := newDeck()
-	hand, remaining := deal(cards, 5)
-
-	if len(hand) != 5 {
-		t.Errorf("expected hand length of 5, got %d", len(hand))
-	}
-
-	if len(remaining) != 47 {
-		t.Errorf("expected remaining deck length of 47, got %d", len(remaining))
-	}
-
-	if hand[0] != "Ace of Spades" {
-		t.Errorf("expected first hand card to be Ace of Spades, got %q", hand[0])
-	}
-
-	if remaining[0] != "Six of Spades" {
-		t.Errorf("expected first remaining card to be Six of Spades, got %q", remaining[0])
-	}
-}
-
-func TestToString(t *testing.T) {
-	cards := deck{"Ace of Spades", "Two of Hearts"}
-	got := cards.toString()
-
-	if got != "Ace of Spades,Two of Hearts" {
-		t.Errorf("expected joined deck string, got %q", got)
-	}
-}
-
 func TestSaveToFileAndNewDeckFromFile(t *testing.T) {
-	filename := "test_deck.txt"
-	cards := deck{"Ace of Spades", "Two of Hearts"}
+	os.Remove("test_deck.txt") // clean up before test
 
-	if err := cards.saveToFile(filename); err != nil {
-		t.Errorf("saveToFile returned error: %v", err)
+	deck := newDeck()
+	deck.saveToFile("test_deck.txt")
+
+	loadedDeck := newDeckFromFile("test_deck.txt")
+
+	if len(loadedDeck) != 52 {
+		t.Errorf("expected loaded deck length of 52, got %d", len(loadedDeck))
 	}
-	defer os.Remove(filename)
 
-	loaded := newDeckFromFile(filename)
-	if !reflect.DeepEqual(loaded, cards) {
-		t.Errorf("expected loaded deck %v, got %v", cards, loaded)
-	}
-}
-
-func TestShuffleChangesOrder(t *testing.T) {
-	cards := newDeck()
-	original := append(deck(nil), cards...)
-
-	shuffleWithRand(&cards, rand.New(rand.NewSource(42)))
-
-	if reflect.DeepEqual(cards, original) {
-		t.Errorf("expected shuffled deck to differ from the original order")
-	}
+	os.Remove("test_deck.txt") // clean up after test
 }
