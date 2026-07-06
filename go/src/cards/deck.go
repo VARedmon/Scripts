fc22dc415b7463c0efdeb95f8c56583a8711590b
vinnie <vincent.redmon@gmail.com>
2026-07-06 15:39:43 -0400

    added testing and go.mod file

--- go/src/cards/deck.go
+++ go/src/cards/deck.go
@@ -1,6 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"os"
+	"strings"
+	"time"
+)
 
 // Create a new type of 'deck' which is a slice of strings
 // which is a slice of strings that represents a deck of cards
@@ -25,5 +31,39 @@ func newDeck() deck {
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
+	}
+}
+
+func deal(d deck, handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:]
+}
+
+func (d deck) toString() string {
+	return strings.Join([]string(d), ",")
+}
+
+func (d deck) saveToFile(filename string) error {
+	return os.WriteFile(filename, []byte(d.toString()), 0666)
+}
+
+func newDeckFromFile(filename string) deck {
+	bs, err := os.ReadFile(filename)
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+
+	s := strings.Split(string(bs), ",")
+	return deck(s)
+}
+
+func (d *deck) shuffle() {
+	shuffleWithRand(d, rand.New(rand.NewSource(time.Now().UnixNano())))
+}
+
+func shuffleWithRand(d *deck, r *rand.Rand) {
+	for i := len(*d) - 1; i > 0; i-- {
+		j := r.Intn(i + 1)
+		(*d)[i], (*d)[j] = (*d)[j], (*d)[i]
 	}
 }
--- go/src/cards/deck_test.go
+++ go/src/cards/deck_test.go
@@ -1,6 +1,80 @@
 package main
 
-import "testing"
+import (
+	"math/rand"
+	"os"
+	"reflect"
+	"testing"
+)
 
-func TestDeck(t *testing.T) {
+func TestNewDeck(t *testing.T) {
+	cards := newDeck()
+
+	if len(cards) != 52 {
+		t.Fatalf("expected deck length of 52, got %d", len(cards))
+	}
+
+	if cards[0] != "Ace of Spades" {
+		t.Fatalf("expected first card to be Ace of Spades, got %q", cards[0])
+	}
+
+	if cards[len(cards)-1] != "King of Clubs" {
+		t.Fatalf("expected last card to be King of Clubs, got %q", cards[len(cards)-1])
+	}
+}
+
+func TestDeal(t *testing.T) {
+	cards := newDeck()
+	hand, remaining := deal(cards, 5)
+
+	if len(hand) != 5 {
+		t.Fatalf("expected hand length of 5, got %d", len(hand))
+	}
+
+	if len(remaining) != 47 {
+		t.Fatalf("expected remaining deck length of 47, got %d", len(remaining))
+	}
+
+	if hand[0] != "Ace of Spades" {
+		t.Fatalf("expected first hand card to be Ace of Spades, got %q", hand[0])
+	}
+
+	if remaining[0] != "Six of Spades" {
+		t.Fatalf("expected first remaining card to be Six of Spades, got %q", remaining[0])
+	}
+}
+
+func TestToString(t *testing.T) {
+	cards := deck{"Ace of Spades", "Two of Hearts"}
+	got := cards.toString()
+
+	if got != "Ace of Spades,Two of Hearts" {
+		t.Fatalf("expected joined deck string, got %q", got)
+	}
+}
+
+func TestSaveToFileAndNewDeckFromFile(t *testing.T) {
+	filename := "test_deck.txt"
+	cards := deck{"Ace of Spades", "Two of Hearts"}
+
+	if err := cards.saveToFile(filename); err != nil {
+		t.Fatalf("saveToFile returned error: %v", err)
+	}
+	defer os.Remove(filename)
+
+	loaded := newDeckFromFile(filename)
+	if !reflect.DeepEqual(loaded, cards) {
+		t.Fatalf("expected loaded deck %v, got %v", cards, loaded)
+	}
+}
+
+func TestShuffleChangesOrder(t *testing.T) {
+	cards := newDeck()
+	original := append(deck(nil), cards...)
+
+	shuffleWithRand(&cards, rand.New(rand.NewSource(42)))
+
+	if reflect.DeepEqual(cards, original) {
+		t.Fatal("expected shuffled deck to differ from the original order")
+	}
 }
